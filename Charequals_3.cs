1e5a81fc6b11d782e8c2ec9d378af12f30ba1d95
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-19 15:40:41 +0530

    To compare characters

---
+++ Charequals_3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charequals_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool result;
+            char ch1 = 'G';
+
+            // checking if 'G' is equal or not
+
+            // Here we are passing char G as the
+            // parameter to the Equals Method
+            result = ch1.Equals('G');
+
+            Console.WriteLine(result);
+
+            // checking if 'v' is equal or not
+            char ch2 = 'v';
+
+            // Here we are passing char W as the
+            // parameter to the Equals Method
+            result = ch2.Equals('W');
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
