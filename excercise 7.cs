ab0c826e8a96f2a69c585707a3e8d60fba09f41d
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-20 13:04:12 +0530

    Displaying data in a pattern

---
+++ excercise 7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excercise_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x;
+            Console.Write("Enter the number ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("{0} {0} {0} {0}\n {0}{0}{0}{0} \n {0} {0} {0} {0} \n {0}{0}{0}{0} ", x);
+           
+
+            int y;
+            Console.Write("\n Enter the number ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}{0}{0} \n{0} {0} \n{0} {0} \n{0} {0} \n{0}{0}{0}" ,y);
+            Console.Read();
+        }
+    }
+}
