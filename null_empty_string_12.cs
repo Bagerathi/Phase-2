b6b8cdc406547f86e30283d4c4de2b50285a5048
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-21 22:06:38 +0530

    to check if string is null/empty

---
+++ null_empty_string_12.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace null_empty_string_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            string str1 = string.Empty;
+            string str2;
+            Console.WriteLine("enter first number :");
+            string a=Console.ReadLine();
+            Console.WriteLine("enter second number: ");
+            string b = Console.ReadLine();
+            bool b1 = String.IsNullOrEmpty(a);
+            Console.WriteLine(b1);
+            bool b2 = String.IsNullOrEmpty(b);
+            Console.WriteLine(b2);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
