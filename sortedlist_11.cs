a778cf4a80d700e695889ad3c5b43e3f72a532e7
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-22 21:03:34 +0530

    Sorting list

---
+++ sortedlist_11.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortedlist_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList s = new SortedList();
+            s.Add(75, "My");
+            s.Add(26, "name");
+            s.Add(18, "xyz");
+            foreach (DictionaryEntry item in s)
+            {
+                Console.WriteLine(item.Key + "  " + item.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
