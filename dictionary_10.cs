b8f11b73033249df575f99961498495c6f466f39
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-22 20:58:44 +0530

    Add files via upload

---
+++ dictionary_10.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            dict.Add(1, "Vicky");
+            dict.Add(2, "Megha");
+            dict.Add(3, "Guru");
+            Console.WriteLine("Values in Dictionary are: \n");
+            foreach (KeyValuePair<int, string> item in dict)
+            {
+                Console.WriteLine(item.Key + "  " + item.Value);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
