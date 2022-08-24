9b72523e661615b3271e454bfc8b1006e7bfcb5d
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 20:53:37 +0530

    Program7

---
+++ Aug-24/Datetime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicOfTheApp
+{
+    public class Datetime
+    { 
+        public static void displaydata()
+        {
+            DateTime dt = DateTime.Now;
+            Console.WriteLine("Current year is: " + dt.Year);
+            Console.WriteLine("Current month is:" + dt.Month);
+            Console.WriteLine("current day is:" +dt.Day);
+            Console.WriteLine("Current hour is: " + dt.Hour);
+            Console.WriteLine( "Current minute is:" +dt.Minute);
+            Console.WriteLine("Current second is :" + dt.Second);
+            Console.WriteLine( "in milliseconds:" +dt.Millisecond); 
+
+        }
+    }
+}
