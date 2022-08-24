22cfba82e272ae44dcf5afed96aacf7b0cb3c6ce
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 20:49:48 +0530

    Program2

---
+++ Aug-24/copystring.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicOfTheApp
+{
+    public class copystring
+    {
+        public static  void createcopy()
+        {
+            string s1 = "Hello ";
+            string s2 = string.Copy(s1);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
