c9850fc6f9f62b029d1317d4b8091adb28b7f6b7
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 13:04:17 +0530

    12th program to check nullorempty

---
+++ Aug-18/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excercise_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        {
+           
+            {
+
+                string nullStr = null;
+                // print True
+                Console.WriteLine(String.IsNullOrEmpty(nullStr));
+
+                string emptyStr1 = "";
+                // print True
+                Console.WriteLine(String.IsNullOrEmpty(emptyStr1));
+
+                string emptyStr2 = String.Empty;
+                // print True
+                Console.WriteLine(String.IsNullOrEmpty(emptyStr2));
+
+                string str = "some text";
+                // print False
+                Console.WriteLine(String.IsNullOrEmpty(str));
+                    Console.Read();
+
+
+            }
+        }
+
+    }
+    }
+}
