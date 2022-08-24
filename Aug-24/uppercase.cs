513b383dd8790c4af8c353428f72c890cf92d1f3
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 20:52:10 +0530

    Program5

---
+++ Aug-24/uppercase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicOfTheApp
+{
+    public  class uppercase
+    {
+        public static void createupper(string s1)
+
+        {
+          
+            string upperstr1 = s1.ToUpper();
+
+            Console.WriteLine(upperstr1);
+        }
+    }
+}
