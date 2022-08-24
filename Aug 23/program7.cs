9c6af2ce5e315453c5aad9f2283285ee5ba22fe6
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 09:01:44 +0530

    Divide by zero exception

---
+++ Aug 23/program7.cs
@@ -0,0 +1,30 @@
+﻿/*
+Write a program to handle Divide By Zero exception and call the stack trace to get the line number
+ */
+
+using System;
+
+namespace Aug23Exercises
+{
+    internal class Prob7
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter Numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int div = a / b;
+                Console.WriteLine(div);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
