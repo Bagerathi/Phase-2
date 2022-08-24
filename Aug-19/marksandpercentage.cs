d790ffc2de2f30e4106978a418a93f03f8568fe0
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 11:51:43 +0530

    calculate total marks and percentage conditions

---
+++ Aug-19/marksandpercentage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace marksandpercentage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grd1 ;
+            double grd2 ;
+            double grd3 ;
+            double total;
+
+            Console.WriteLine("Enter your first grade: ");
+            grd1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your second grade: ");
+            grd2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your third grade: ");
+            grd3 = double.Parse(Console.ReadLine());
+
+            total= grd1 + grd2 + grd3;
+            if (total >= 60)
+            {
+                Console.WriteLine("You are Fisrt! Your average is: " + total);
+            }
+            else if (total>= 45)
+            {
+                Console.WriteLine("Your total is: " + total);
+
+
+            }
+            else 
+            {
+                Console.WriteLine("fail");
+                    
+                    }
+            Console.ReadLine();
+        }
+    }
+}
