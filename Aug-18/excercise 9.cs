a75b1de41c2d52ab825b8805ab90c681d7cf8617
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 13:02:38 +0530

    Celsius to  Kelvin and  Fahrenheit.

---
+++ Aug-18/excercise 9.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excercise_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, kelvin, fahrenheit;
+            Console.Write("Enter the amount of Celsius: ");
+            celsius = Convert.ToDouble(Console.ReadLine());
+            kelvin = celsius + 273;
+            fahrenheit = celsius * 18 / 10 + 32;
+            Console.WriteLine("Kelvin = {0}K",+kelvin);
+            Console.WriteLine("Fahrenheit = {0}F",+fahrenheit);
+            Console.Read();
+
+        }
+
+    }
+    
+}
