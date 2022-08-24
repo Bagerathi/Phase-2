1d760c3a32ea0f3c2774e9e0ae2489c188086844
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 20:51:39 +0530

    Program4

---
+++ Aug-24/stringseperator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicOfTheApp
+{
+    public class stringseperator
+    {
+        public static void split()
+        {
+            string s1 = "Welcome to the world of C# Programming";
+            string[] s2 = s1.Split('#');
+            foreach (string s3 in s2)
+            {
+                Console.WriteLine(s3);
+            }
+        }
+    }
+}
