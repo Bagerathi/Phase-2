5eb6d1f6656438d74635a5d4232678cdad432fef
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 11:49:34 +0530

    to check given strings are equal

---
+++ Aug-19/twostringsequal_4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twostringsequal_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str1 = "vicky";
+            string str2 = "vicky";
+
+            // Here we use == operator to check
+            // the equality of the strings
+            Console.WriteLine(str1 == str2);
+            Console.ReadLine();
+        }
+    }
+}
