8ad9a9119e6b600a1f9a9d7612ca5f484c67aafa
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 20:52:58 +0530

    Program6

---
+++ Aug-24/blanktextfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace LogicOfTheApp
+{
+    public class blanktextfile
+    {
+        public static void createfile()
+        {
+            string Path = @"D:\\Phase2\\C# day5 aug 24\\filehandling_demo\text.txt";
+            File.Create(Path);
+
+        }
+    }
+}
