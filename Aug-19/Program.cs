f71a624f45f8fad2529cfebb014eb3b9c62d0f12
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 11:50:14 +0530

    Program 5

---
+++ Aug-19/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biggestintwonubers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 50;
+            int num2 = 90;
+            int maxNum;
+            Console.WriteLine("Number 1: " + num1);
+            Console.WriteLine("Number 2: " + num2);
+            if (num1 > num2)
+            {
+                maxNum = num1;
+            }
+            else
+            {
+                maxNum = num2;
+            }
+            Console.WriteLine(" Biggest number is: " + maxNum);
+            Console.ReadKey();
+        }
+    }
+}
