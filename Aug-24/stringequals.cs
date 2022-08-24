78074fb96dba9a7d4d38c9f63ab45222752b277a
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 20:48:46 +0530

    program1

---
+++ Aug-24/stringequals.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicOfTheApp
+{
+    public class stringequals
+    {
+
+        public static void stringequal()
+        {
+            string s1 = "hello";
+            string s2 = "hello";
+
+            // Equals() method return true
+            // as both string objects are equal
+            Console.WriteLine(s1.Equals(s2));
+
+        }
+    }
+}
