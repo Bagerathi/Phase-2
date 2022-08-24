05ed89d1f6ee96b978c2adae1fb999c874a4ff32
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 11:57:39 +0530

    15th program

---
+++ Aug-19/forloops_15.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forloops_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, j = 25;
+
+            for (i = 0, j = 25; i < 25 && j >= 0; i++, j--)
+            {
+                if (i > j)
+                {
+                    Console.WriteLine("Crossed Over");
+                    break;
+                }
+                Console.WriteLine("{0} {1}", i, j);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
