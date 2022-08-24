7d7261fc8d5dffc6549fe340cf2b22a99a88b139
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 11:56:51 +0530

    13th program

---
+++ Aug-19/multiple_if_13.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiple_if_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the number");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num == 0)
+                Console.WriteLine("number is zero");
+            else if (num % 2 == 0)
+            {
+                Console.WriteLine("even");
+                if (num % 10 == 0)
+                {
+                    Console.WriteLine("{0} is a multiple of 10", num);
+                }
+                else
+                {
+                    Console.WriteLine("{0 is }not a multiple of 10", num);
+                }
+                if (num > 100)
+                    Console.WriteLine("too large");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+                Console.WriteLine("{0} is not a multiple of 10", num);
+                if (num > 100)
+                    Console.WriteLine("out of bounds");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
