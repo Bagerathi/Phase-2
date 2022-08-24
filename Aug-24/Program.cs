49aa7a3051c4d4d123eeb2779989c93f05f3264e
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-24 20:47:22 +0530

    available inside Filehandlingdemo
    The program.cs is inside the Filehandlingdemo console application where the classes are called
    
    the logics are written seperately in a dll file named LogicOfTheApp  and this can contain numerous classes

---
+++ Aug-24/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogicOfTheApp;
+
+namespace filehandling_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleClass.Hello();
+
+            DifferentFileMethods.MakeFile();
+
+
+            string s =Console.ReadLine();
+            UnderstandingStrings.StringLength(s);
+
+               
+            stringequals.stringequal();
+
+            copystring.createcopy();
+
+            indexof.createindex();
+
+
+            stringseperator.split();
+
+
+
+            Console.WriteLine("===================");
+            Console.WriteLine("enter to convert to upper");
+            string s1=Console.ReadLine();
+
+            uppercase.createupper(s1);
+
+
+            Console.WriteLine("====================");
+            blanktextfile.createfile();
+            Console.WriteLine("Textfile created");
+
+            Console.WriteLine("=======================");
+            Datetime.displaydata();
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
