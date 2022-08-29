42648f8e12a1c5710b16d335c683073159bb6913
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-29 12:34:46 +0530

    Add files via upload

---
+++ Aug-26/studentslistprogram3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using studentsLibrary;
+
+
+namespace datastructures_array
+{
+    internal class studentslistprogram3
+    {
+        static void Main(string[] args)
+        {
+            List<Studentdetails> students = new List<Studentdetails>();
+            {
+                new Studentdetails() { group = "Bio", marks = 42 };
+                new Studentdetails() { group = "Commerce", marks = 34 };
+               };
+
+            for (int i = 0; i < Studentdetails.len; i++)
+            {
+                Console.WriteLine(Studentdetails[i].Name + " is " + Studentdetails[i].Age + " years old");
+            }
+
+
+        }
+    }
+}
