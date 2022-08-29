f003045a60829704a94d11289fd1c1c0cb9da678
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-08-29 12:33:56 +0530

    Add files via upload

---
+++ Aug-26/studentarrayprog1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRLibrary;
+using studentsLibrary;
+
+
+namespace datastructures_array
+{
+    internal class studentarrayprog1
+    {
+        static void Main(string[] args)
+        {
+           
+            Student[] stud = { new Student(),
+                       new Student(),
+                       new Student() };
+
+            
+            stud[0].SetStudent(1, "vicky", 20);
+            stud[1].SetStudent(2, "Sam", 21);
+            stud[2].SetStudent(3, "JO", 23);
+
+            // Call the display method
+            stud[0].DisplayStudent();
+            stud[1].DisplayStudent();
+            stud[2].DisplayStudent();
+
+            Console.ReadLine();
+
+        }
+
+        
+    }
+}
