cba5752a516df32c597c8a6fd569f230b707e850
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-09-06 23:22:57 +0530

    Main program.cs

---
+++ Sept-5/program.cs
@@ -0,0 +1,31 @@
+using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sept5_handson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cart c = new Cart();
+            c.AcceptData();
+            c.AcceptData();
+            List<Book> Blist = new List<Book>();
+            Blist = c.BooksList();
+
+            foreach (var i in Blist)
+            {
+                Console.WriteLine("Book ID: " + i.BookID);
+                Console.WriteLine("Book Name: " + i.BookName);
+                Console.WriteLine("Book Price: " + i.Price);
+                c.BookCount();
+            }
+            Console.ReadLine();
+        }
+    }
+}
