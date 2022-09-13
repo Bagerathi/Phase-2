e740f6d93e95d3dcd06c73937a440fcba7e06c2c
Bagerathi <73739871+Bagerathi@users.noreply.github.com>
2022-09-13 12:33:10 +0530

    A Class file in PART-B

---
+++ Sept-12/StereoDiscAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sept12_handsonCustomAtt
+{
+
+
+   [System.AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    sealed class DeveloperAttribute : Attribute
+    {
+
+        public DeveloperAttribute(string _songname)
+        {
+
+            this.SongName = _songname;
+
+        }
+        public string SongName { get; set; }
+        public string Language { get; set; }
+        public string Lyrics { get; set; }
+    }
+
+
+}
